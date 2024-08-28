ee43e3d67cfa20cf03d752ad31711adc50ad5b3e
Ed Solovey <ed@digits.com>
2024-08-28 12:40:34 -0400

    future proof MockFinHubClient

--- src/main/java/edu/bu/finhub/MockFinhubClient.java
+++ src/main/java/edu/bu/finhub/MockFinhubClient.java
@@ -4,8 +4,11 @@ import com.google.common.collect.ImmutableList;
 import edu.bu.data.DataStore;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.stream.Collectors;
 import org.tinylog.Logger;
 
 /**
@@ -17,30 +20,44 @@ public class MockFinhubClient implements StockUpdatesClient {
   static final String MS_BETWEEN_CALLS_ARGUMENT = "msBetweenCalls";
   static final long MS_BETWEEN_CALLS_DEFAULT = 5000;
   // the time of these responses is not used - this mock will generate timestamps mimicing the time
-  // at which
-  // it is executed
-  static final List<FinhubResponse> CANNED_RESPONSES =
+  // at which it is executed
+  static final List<CannedResponse> CANNED_RESPONSES =
       ImmutableList.of(
-          new FinhubResponse("RIVN", 18.0, 0, 8),
-          new FinhubResponse("BAC", 42.0, 0, 15),
-          new FinhubResponse("NKE", 71.23, 0, 20),
-          new FinhubResponse("NKE", 72.10, 0, 8),
-          new FinhubResponse("BAC", 41.88, 0, 10),
-          new FinhubResponse("RIVN", 18.5, 0, 30),
-          new FinhubResponse("RIVN", 18.7, 0, 10),
-          new FinhubResponse("RIVN", 18.1, 0, 11),
-          new FinhubResponse("BAC", 41.91, 0, 3),
-          new FinhubResponse("BAC", 41.11, 0, 12),
-          new FinhubResponse("NKE", 73.0, 0, 11),
-          new FinhubResponse("RIVN", 19.0, 0, 7),
-          new FinhubResponse("NKE", 71.8, 0, 30),
-          new FinhubResponse("BAC", 43.1, 0, 10));
+          new CannedResponse(18.0, 8),
+          new CannedResponse(42.0, 15),
+          new CannedResponse(71.23, 20),
+          new CannedResponse(72.10, 8),
+          new CannedResponse(41.88, 10),
+          new CannedResponse(18.5, 30),
+          new CannedResponse(18.7, 10),
+          new CannedResponse(18.1, 11),
+          new CannedResponse(41.91, 3),
+          new CannedResponse(41.11, 12),
+          new CannedResponse(73.0, 11),
+          new CannedResponse(19.0, 7),
+          new CannedResponse(71.8, 30),
+          new CannedResponse(43.1, 10));
   final DataStore store;
   final Set<String> arguments;
+
+  final Set<String> subscribedSymbols;
+
+  // these variables help us round-robin canned responses through known stock symbols
+  List<String> symbolOrder;
+  int responsesCount = 0;
 
   public MockFinhubClient(DataStore store, Set<String> arguments) {
     this.store = store;
     this.arguments = arguments;
+
+    this.subscribedSymbols = new ConcurrentSkipListSet<>();
+
+    // start with some subscribed symbols in deterministic order to support tests that won't modify
+    // subscribed set
+    addSymbol("RIVN");
+    addSymbol("BAC");
+    addSymbol("NKE");
+    symbolOrder = List.of("RIVN", "BAC", "NKE");
   }
 
   @Override
@@ -53,18 +70,26 @@ public class MockFinhubClient implements StockUpdatesClient {
 
               Instant responseTime = Instant.now();
 
-              for (FinhubResponse response : CANNED_RESPONSES) {
-                FinhubResponse actualResponse =
-                    new FinhubResponse(
-                        response.symbol,
-                        response.price,
-                        responseTime.toEpochMilli(),
-                        response.volume);
+              for (CannedResponse cannedResponse : CANNED_RESPONSES) {
+                if (symbolOrder.isEmpty()) {
+                  Logger.warn(
+                      "Mock Finhub Client cannot emit a canned response because there are zero symbols registered.");
+                } else {
+                  String nextSymbol = symbolOrder.get(responsesCount % symbolOrder.size());
 
-                Logger.info("processing mock response: " + actualResponse);
-                store.update(ImmutableList.of(actualResponse));
+                  FinhubResponse actualResponse =
+                      new FinhubResponse(
+                          nextSymbol,
+                          cannedResponse.price,
+                          responseTime.toEpochMilli(),
+                          cannedResponse.volume);
 
-                responseTime = responseTime.plus(1, ChronoUnit.SECONDS);
+                  Logger.info("processing mock response: " + actualResponse);
+                  store.update(ImmutableList.of(actualResponse));
+
+                  responsesCount++;
+                  responseTime = responseTime.plus(1, ChronoUnit.SECONDS);
+                }
 
                 try {
                   Thread.sleep(msBetweenCalls);
@@ -85,5 +110,30 @@ public class MockFinhubClient implements StockUpdatesClient {
     }
 
     return MS_BETWEEN_CALLS_DEFAULT;
+  }
+
+  public void addSymbol(String symbol) {
+    subscribedSymbols.add(symbol);
+    symbolOrder = subscribedSymbols.stream().collect(Collectors.toList());
+  }
+
+  public void removeSymbol(String symbol) {
+    subscribedSymbols.remove(symbol);
+    symbolOrder = subscribedSymbols.stream().collect(Collectors.toList());
+  }
+
+  public Set<String> subscribedSymbols() {
+    return Collections.unmodifiableSet(subscribedSymbols);
+  }
+
+  /** Simple pair of price and volume used to configure canned responses. */
+  static class CannedResponse {
+    final double price;
+    final long volume;
+
+    public CannedResponse(double price, long volume) {
+      this.price = price;
+      this.volume = volume;
+    }
   }
 }
