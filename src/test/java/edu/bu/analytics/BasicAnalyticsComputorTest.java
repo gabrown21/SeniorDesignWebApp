a279b66b2d552d7435bb4e4181b1700b600e6040
gabrown21 <gabrown@bu.edu>
2024-09-16 11:24:05 -0400

    Finished implementing the other test cases for problem 1.1 and used statusApply to fix format

--- src/test/java/edu/bu/analytics/BasicAnalyticsComputorTest.java
+++ src/test/java/edu/bu/analytics/BasicAnalyticsComputorTest.java
@@ -1,5 +1,7 @@
 package edu.bu.analytics;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import com.google.common.collect.ImmutableList;
 import edu.bu.data.DataStore;
 import edu.bu.data.InMemoryStore;
@@ -9,8 +11,6 @@ import java.time.temporal.ChronoUnit;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class BasicAnalyticsComputorTest {
 
@@ -65,14 +65,15 @@ public class BasicAnalyticsComputorTest {
   @Test
   public void totalObservedVolume_uknownSymbol() {
     UnknownSymbolException exception =
-            assertThrows(UnknownSymbolException.class, () -> analyticsComputor.totalObservedVolume("NVDA"));
+        assertThrows(
+            UnknownSymbolException.class, () -> analyticsComputor.totalObservedVolume("NVDA"));
     assertEquals("NVDA has not been seen by the server", exception.getMessage());
   }
 
   @Test
   public void totalObservedVolume_oneDataPoint() throws UnknownSymbolException {
     List<FinhubResponse> singleResponse =
-            ImmutableList.of(new FinhubResponse("NVDA", 134.12, TEST_TIME.toEpochMilli(), 100));
+        ImmutableList.of(new FinhubResponse("NVDA", 134.12, TEST_TIME.toEpochMilli(), 100));
 
     dataStore.update(singleResponse);
     assertEquals(100, analyticsComputor.totalObservedVolume("NVDA"));
@@ -81,65 +82,104 @@ public class BasicAnalyticsComputorTest {
   @Test
   public void totalObservedVolume_multipleMixedDataPoints() throws UnknownSymbolException {
     dataStore.update(
-            ImmutableList.of(new FinhubResponse("NVDA", 134.12, TEST_TIME.toEpochMilli(), 100)));
+        ImmutableList.of(new FinhubResponse("NVDA", 134.12, TEST_TIME.toEpochMilli(), 100)));
 
-    // second response 5 seconds later
     dataStore.update(
-            ImmutableList.of(new FinhubResponse(
-                            "NVDA", 135.33, TEST_TIME.plus(5, ChronoUnit.SECONDS).toEpochMilli(), 100)));
-
-    // third response 13 seconds later
+        ImmutableList.of(
+            new FinhubResponse(
+                "NVDA", 135.33, TEST_TIME.plus(5, ChronoUnit.SECONDS).toEpochMilli(), 100)));
     dataStore.update(
-            ImmutableList.of(new FinhubResponse(
-                            "NVDA", 135.01, TEST_TIME.plus(13, ChronoUnit.SECONDS).toEpochMilli(), 100)));
-    //100+100+100 = 300
+        ImmutableList.of(
+            new FinhubResponse(
+                "NVDA", 135.01, TEST_TIME.plus(13, ChronoUnit.SECONDS).toEpochMilli(), 100)));
+    // 100+100+100 = 300
     assertEquals(300, analyticsComputor.totalObservedVolume("NVDA"));
   }
 
   @Test
   public void mostActiveStock_noData() {
-    //Should just be null for noData
+    // Should just be null for noData
     assertNull(analyticsComputor.mostActiveStock());
   }
 
   @Test
   public void mostActiveStock_singleDataPoint() {
     dataStore.update(
-            ImmutableList.of(new FinhubResponse
-                    ("NVDA", 134.12, TEST_TIME.toEpochMilli(), 100)));
-    //Should be just NVDA so must be most active
+        ImmutableList.of(new FinhubResponse("NVDA", 134.12, TEST_TIME.toEpochMilli(), 100)));
+    // Should be just NVDA so must be most active
     assertEquals("NVDA", analyticsComputor.mostActiveStock());
   }
 
   @Test
   public void mostActiveStock_multpleStocks_largestVolumeNotMostDataPoints() {
     dataStore.update(
-            ImmutableList.of(new FinhubResponse("NVDA", 134.12, TEST_TIME.toEpochMilli(),100)));
+        ImmutableList.of(new FinhubResponse("NVDA", 134.12, TEST_TIME.toEpochMilli(), 100)));
     dataStore.update(
-            ImmutableList.of(new FinhubResponse("NVDA", 134.12, TEST_TIME.toEpochMilli(),100)));
+        ImmutableList.of(
+            new FinhubResponse(
+                "NVDA", 134.12, TEST_TIME.plus(5, ChronoUnit.SECONDS).toEpochMilli(), 100)));
     dataStore.update(
-            ImmutableList.of(new FinhubResponse("TSLA", 300, TEST_TIME.toEpochMilli(),300)));
-
+        ImmutableList.of(
+            new FinhubResponse(
+                "TSLA", 300, TEST_TIME.plus(13, ChronoUnit.SECONDS).toEpochMilli(), 300)));
+    // 300>268.24
     assertEquals("TSLA", analyticsComputor.mostActiveStock());
   }
 
   @Test
   public void mostActiveStock_multpleStocks_largestVolumeAlsoMostDataPoints() {
-    // TODO: implement
+    dataStore.update(
+        ImmutableList.of(new FinhubResponse("NVDA", 134.12, TEST_TIME.toEpochMilli(), 100)));
+    dataStore.update(
+        ImmutableList.of(
+            new FinhubResponse(
+                "NVDA", 134.12, TEST_TIME.plus(5, ChronoUnit.SECONDS).toEpochMilli(), 100)));
+    dataStore.update(
+        ImmutableList.of(
+            new FinhubResponse(
+                "TSLA", 300, TEST_TIME.plus(13, ChronoUnit.SECONDS).toEpochMilli(), 100)));
+    dataStore.update(
+        ImmutableList.of(
+            new FinhubResponse(
+                "TSLA", 300, TEST_TIME.plus(20, ChronoUnit.SECONDS).toEpochMilli(), 100)));
+    dataStore.update(
+        ImmutableList.of(
+            new FinhubResponse(
+                "TSLA", 300, TEST_TIME.plus(27, ChronoUnit.SECONDS).toEpochMilli(), 100)));
+    // TSLA volume = 300 > NVDA volume of 200 & More TSLA Data points
+    assertEquals("TSLA", analyticsComputor.mostActiveStock());
   }
 
   @Test
   public void knownSymbols_empty() {
-    // TODO: implement
+    assertTrue(analyticsComputor.knownSymbols().isEmpty());
   }
 
   @Test
   public void knownSymbols_single() {
-    // TODO: implement
+    dataStore.update(
+        ImmutableList.of(new FinhubResponse("NVDA", 134.12, TEST_TIME.toEpochMilli(), 100)));
+    // Cannot compare string to Set<String>
+    assertTrue(analyticsComputor.knownSymbols().contains("NVDA"));
   }
 
   @Test
   public void knownSymbols_multiple() {
-    // TODO: implement
+    dataStore.update(
+        ImmutableList.of(new FinhubResponse("NVDA", 134.12, TEST_TIME.toEpochMilli(), 100)));
+
+    dataStore.update(
+        ImmutableList.of(
+            new FinhubResponse(
+                "AAPL", 300, TEST_TIME.plus(5, ChronoUnit.SECONDS).toEpochMilli(), 100)));
+
+    dataStore.update(
+        ImmutableList.of(
+            new FinhubResponse(
+                "TSLA", 290, TEST_TIME.plus(13, ChronoUnit.SECONDS).toEpochMilli(), 100)));
+
+    assertTrue(analyticsComputor.knownSymbols().contains("AAPL"));
+    assertTrue(analyticsComputor.knownSymbols().contains("TSLA"));
+    assertTrue(analyticsComputor.knownSymbols().contains("NVDA"));
   }
 }
