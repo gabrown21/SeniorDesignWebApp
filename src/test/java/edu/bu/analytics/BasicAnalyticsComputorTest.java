7d77787c8a19a323f3d2d13fe68ef98923904bcf
gabrown21 <gabrown@bu.edu>
2024-09-16 10:57:25 -0400

    Completed writing initial 6 tests for Problem 1.1.

--- src/test/java/edu/bu/analytics/BasicAnalyticsComputorTest.java
+++ src/test/java/edu/bu/analytics/BasicAnalyticsComputorTest.java
@@ -1,8 +1,5 @@
 package edu.bu.analytics;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import com.google.common.collect.ImmutableList;
 import edu.bu.data.DataStore;
 import edu.bu.data.InMemoryStore;
@@ -12,6 +9,8 @@ import java.time.temporal.ChronoUnit;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BasicAnalyticsComputorTest {
 
@@ -65,32 +64,63 @@ public class BasicAnalyticsComputorTest {
 
   @Test
   public void totalObservedVolume_uknownSymbol() {
-    // TODO: implement
+    UnknownSymbolException exception =
+            assertThrows(UnknownSymbolException.class, () -> analyticsComputor.totalObservedVolume("NVDA"));
+    assertEquals("NVDA has not been seen by the server", exception.getMessage());
   }
 
   @Test
   public void totalObservedVolume_oneDataPoint() throws UnknownSymbolException {
-    // TODO: implement
+    List<FinhubResponse> singleResponse =
+            ImmutableList.of(new FinhubResponse("NVDA", 134.12, TEST_TIME.toEpochMilli(), 100));
+
+    dataStore.update(singleResponse);
+    assertEquals(100, analyticsComputor.totalObservedVolume("NVDA"));
   }
 
   @Test
   public void totalObservedVolume_multipleMixedDataPoints() throws UnknownSymbolException {
-    // TODO: implement
+    dataStore.update(
+            ImmutableList.of(new FinhubResponse("NVDA", 134.12, TEST_TIME.toEpochMilli(), 100)));
+
+    // second response 5 seconds later
+    dataStore.update(
+            ImmutableList.of(new FinhubResponse(
+                            "NVDA", 135.33, TEST_TIME.plus(5, ChronoUnit.SECONDS).toEpochMilli(), 100)));
+
+    // third response 13 seconds later
+    dataStore.update(
+            ImmutableList.of(new FinhubResponse(
+                            "NVDA", 135.01, TEST_TIME.plus(13, ChronoUnit.SECONDS).toEpochMilli(), 100)));
+    //100+100+100 = 300
+    assertEquals(300, analyticsComputor.totalObservedVolume("NVDA"));
   }
 
   @Test
   public void mostActiveStock_noData() {
-    // TODO: implement
+    //Should just be null for noData
+    assertNull(analyticsComputor.mostActiveStock());
   }
 
   @Test
   public void mostActiveStock_singleDataPoint() {
-    // TODO: implement
+    dataStore.update(
+            ImmutableList.of(new FinhubResponse
+                    ("NVDA", 134.12, TEST_TIME.toEpochMilli(), 100)));
+    //Should be just NVDA so must be most active
+    assertEquals("NVDA", analyticsComputor.mostActiveStock());
   }
 
   @Test
   public void mostActiveStock_multpleStocks_largestVolumeNotMostDataPoints() {
-    // TODO: implement
+    dataStore.update(
+            ImmutableList.of(new FinhubResponse("NVDA", 134.12, TEST_TIME.toEpochMilli(),100)));
+    dataStore.update(
+            ImmutableList.of(new FinhubResponse("NVDA", 134.12, TEST_TIME.toEpochMilli(),100)));
+    dataStore.update(
+            ImmutableList.of(new FinhubResponse("TSLA", 300, TEST_TIME.toEpochMilli(),300)));
+
+    assertEquals("TSLA", analyticsComputor.mostActiveStock());
   }
 
   @Test
