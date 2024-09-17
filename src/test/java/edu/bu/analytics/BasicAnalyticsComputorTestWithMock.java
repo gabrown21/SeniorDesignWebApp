2cf476f578e9dc51ad985b60d8e1ee1efd788fc7
gabrown21 <gabrown@bu.edu>
2024-09-16 20:41:34 -0400

    Implemented first couple mock tests correctly

--- src/test/java/edu/bu/analytics/BasicAnalyticsComputorTestWithMock.java
+++ src/test/java/edu/bu/analytics/BasicAnalyticsComputorTestWithMock.java
@@ -1,15 +1,17 @@
 package edu.bu.analytics;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import edu.bu.data.DataStore;
 import edu.bu.finhub.FinhubResponse;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Collections;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -62,22 +64,47 @@ public class BasicAnalyticsComputorTestWithMock {
 
   @Test
   public void totalObservedVolume_uknownSymbol() {
-    // TODO: implement
+    when(dataStore.haveSymbol("NVDA")).thenReturn(false);
+
+    UnknownSymbolException exception =
+            assertThrows(UnknownSymbolException.class, () -> basicAnalyticsComputor.totalObservedVolume("NVDA"));
+
+    assertEquals("NVDA has not been seen by the server", exception.getMessage());
   }
 
   @Test
   public void totalObservedVolume_oneDataPoint() throws UnknownSymbolException {
-    // TODO: implement
+    when(dataStore.haveSymbol("NVDA")).thenReturn(true);
+
+    when(dataStore.getHistory("NVDA"))
+            .thenReturn(ImmutableList.of(
+                    new FinhubResponse("NVDA", 134.12, TEST_TIME.toEpochMilli(), 100)));
+
+    assertEquals(100, basicAnalyticsComputor.totalObservedVolume("NVDA"));
   }
 
   @Test
   public void totalObservedVolume_multipleMixedDataPoints() throws UnknownSymbolException {
-    // TODO: implement
+    when(dataStore.haveSymbol("NVDA")).thenReturn(true);
+
+    when(dataStore.getHistory("NVDA"))
+            .thenReturn(
+                    ImmutableList.of(
+                            new FinhubResponse(
+                                    "NVDA", 135.01, TEST_TIME.plus(13, ChronoUnit.SECONDS).toEpochMilli(), 100),
+                            new FinhubResponse(
+                                    "NVDA", 135.33, TEST_TIME.plus(5, ChronoUnit.SECONDS).toEpochMilli(), 100),
+                            new FinhubResponse(
+                                    "NVDA", 134.12, TEST_TIME.toEpochMilli(), 100)));
+
+    assertEquals(300, basicAnalyticsComputor.totalObservedVolume("NVDA"));
   }
 
   @Test
   public void mostActiveStock_noData() {
-    // TODO: implement
+    when(dataStore.knownSymbols()).thenReturn(ImmutableSet.of());
+
+    assertNull(basicAnalyticsComputor.mostActiveStock());
   }
 
   @Test
