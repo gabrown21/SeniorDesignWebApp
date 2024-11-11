e6a4db2073e3b677d816d875cb648d3751210d53
gabrown21 <gabrown@bu.edu>
2024-11-11 17:47:20 -0500

    Fixed ServerIntegrationTest to handle java

--- monolith-service/src/test/java/edu/bu/ServerIntegrationTest.java
+++ monolith-service/src/test/java/edu/bu/ServerIntegrationTest.java
@@ -10,8 +10,11 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -28,26 +31,44 @@ public class ServerIntegrationTest {
 
   @Test
   public void requestAgainstRunningServer_knownSymbols()
-      throws URISyntaxException, IOException, InterruptedException {
+      throws URISyntaxException, IOException, InterruptedException, ParseException {
 
     String rawResponse = makeRequest("http://localhost:8000/symbols");
-    Set<String> knownSymbols = new HashSet<>(List.of(rawResponse.split(",")));
-
+    JSONParser parser = new JSONParser();
+    JSONObject parsedResponse = (JSONObject) parser.parse(rawResponse);
+    JSONArray jsonArray = (JSONArray) parsedResponse.get("symbols");
+    Set<String> knownSymbols = new HashSet<>();
+    for (Object element : jsonArray) {
+      knownSymbols.add(element.toString());
+    }
     assertEquals(ImmutableSet.of("BAC", "RIVN", "NKE"), knownSymbols);
   }
 
   @Test
   public void requestAgainstRunningServer_price()
-      throws URISyntaxException, IOException, InterruptedException {
+      throws URISyntaxException, IOException, InterruptedException, ParseException {
     String rawResponse = makeRequest("http://localhost:8000/price/BAC");
+    JSONParser parser = new JSONParser();
+    JSONObject jsonObject = (JSONObject) parser.parse(rawResponse);
+    Object priceObj = jsonObject.get("currentPrice");
+    if (priceObj == null) {
+      throw new IllegalArgumentException("currentPrice key is missing in the JSON response");
+    }
     double price = Double.parseDouble(rawResponse);
     assertEquals(43.1, price, 0.01);
   }
 
   @Test
   public void requestAgainstRunningServer_mostActiveStock()
-      throws URISyntaxException, IOException, InterruptedException {
+      throws URISyntaxException, IOException, InterruptedException, ParseException {
     String rawResponse = makeRequest("http://localhost:8000/mostactive");
+    JSONParser parser = new JSONParser();
+    JSONObject jsonObject = (JSONObject) parser.parse(rawResponse);
+
+    Object symbolObj = jsonObject.get("mostActiveStock");
+    if (symbolObj == null) {
+      throw new IllegalArgumentException("mostActiveStock key is missing in the JSON response");
+    }
     assertEquals("RIVN", rawResponse);
   }
 
