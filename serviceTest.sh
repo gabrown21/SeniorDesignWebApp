b3e16e347ff0fc7afda94d0d1cdf9ac4e1292112
gabrown21 <gabrown@bu.edu>
2024-11-11 11:09:20 -0500

    Added the edits to survice test. Now just need to account for JSON

--- serviceTest.sh
+++ serviceTest.sh
@@ -18,6 +18,8 @@ sleep 5
 echo "Starting Monolith Service in a new process"
 gradle monolith-service:runService &
 monolithServicePid=$!
+# capture output of integration test
+testResult=$?
 echo "Monolith Service PID is $monolithServicePid"
 sleep 15
 
@@ -32,3 +34,6 @@ echo "Stopping all services"
 kill -9 $queueServicePid
 kill -9 $finnhubServicePid
 kill -9 $monolithServicePid
+# exit with result of integration test and not the above kill command which would have been the default
+exit $testResult
+
