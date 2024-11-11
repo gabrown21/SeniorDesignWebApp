0e79a0bc7ebf8800907ac0c0802db3e9a0b580df
gabrown21 <gabrown@bu.edu>
2024-11-11 17:58:13 -0500

    Should be passing integration test and fully working now

--- monolith-service/src/test/java/edu/bu/ServerIntegrationTest.java
+++ monolith-service/src/test/java/edu/bu/ServerIntegrationTest.java
@@ -23,6 +23,7 @@ import org.junit.jupiter.api.Test;
 public class ServerIntegrationTest {
 
   HttpClient httpClient;
+  JSONParser parser = new JSONParser();
 
   @BeforeEach
   public void setUp() {
