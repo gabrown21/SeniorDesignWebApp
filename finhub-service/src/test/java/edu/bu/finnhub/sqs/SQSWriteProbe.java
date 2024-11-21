44f9a578f2223b136cb9fca4b3ed2f9de967d09f
gabrown21 <gabrown@bu.edu>
2024-11-20 22:49:42 -0500

    Implemented Problem 3 and tested. Appears to be working. Had to change how i created sqsClient for it to work

--- finhub-service/src/test/java/edu/bu/finnhub/sqs/SQSWriteProbe.java
+++ finhub-service/src/test/java/edu/bu/finnhub/sqs/SQSWriteProbe.java
@@ -4,25 +4,26 @@ import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.*;
-import java.util.List;
-import org.tinylog.Logger;
 
 @Tag("Probe")
 public class SQSWriteProbe {
     private static final String QUEUE_URL = "https://sqs.us-east-2.amazonaws.com/183631322250/GabrielBrown-Standard1";
     @Test
     public void writeMessage() {
-        SqsClient sqsClient = SqsClient.create();
+        SqsClient sqsClient = SqsClient.builder()
+                .region(Region.US_EAST_2)
+                .build();
+
         for (int i = 1; i <= 4; i++) {
             SendMessageRequest sendMessageRequest = SendMessageRequest.builder()
                     .queueUrl(QUEUE_URL)
-                    .messageBody("Hello world from Gabe's Stock App")
+                    .messageBody("Hello world from Gabe's Stock App" + i)
                     .delaySeconds(5)
                     .build();
 
             SendMessageResponse response = sqsClient.sendMessage(sendMessageRequest);
 
-            Logger.info("Message sent. ID: " + response.messageId());
+            //Logger.info("Message sent. ID: " + response.messageId());
         }
 
         sqsClient.close();
--- monolith-service/src/test/java/edu/bu/sqs/SQSReadProbe.java
+++ monolith-service/src/test/java/edu/bu/sqs/SQSReadProbe.java
@@ -16,26 +16,31 @@ public class SQSReadProbe {
     private static final String EXPECTED_MESSAGE = "Hello world from Gabe's Stock App";
     @Test
     public void readMessage() {
-        SqsClient sqsClient = SqsClient.create();
-        ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
-                .queueUrl(QUEUE_URL)
-                .maxNumberOfMessages(10)
+        SqsClient sqsClient = SqsClient.builder()
+                .region(Region.US_EAST_2)
                 .build();
+        boolean keepReading = true;
+        while(keepReading) {
+            ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
+                    .queueUrl(QUEUE_URL)
+                    .maxNumberOfMessages(5)
+                    .build();
 
-        List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
+            List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
 
-        if (messages.isEmpty()) {
-            Logger.info("No messages in the queue.");
-        } else {
-            for (Message message : messages) {
-                assertEquals(EXPECTED_MESSAGE, message.body());
-                Logger.info("Message received: " + message.body());
-                DeleteMessageRequest deleteMessageRequest = DeleteMessageRequest.builder()
-                        .queueUrl(QUEUE_URL)
-                        .receiptHandle(message.receiptHandle())
-                        .build();
-                sqsClient.deleteMessage(deleteMessageRequest);
-                Logger.info("Message deleted. ID: " + message.messageId());
+            if (messages.isEmpty()) {
+                Logger.info("No messages in the queue.");
+                keepReading = false;
+            } else {
+                for (Message message : messages) {
+                    Logger.info("Message received: " + message.body());
+                    DeleteMessageRequest deleteMessageRequest = DeleteMessageRequest.builder()
+                            .queueUrl(QUEUE_URL)
+                            .receiptHandle(message.receiptHandle())
+                            .build();
+                    sqsClient.deleteMessage(deleteMessageRequest);
+                    Logger.info("Message deleted. ID: " + message.messageId());
+                }
             }
         }
         sqsClient.close();
